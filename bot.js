d9116ad4ea8e1a82f10cd4b39f6d8313d561cca5
Cassandra McCarthy <cassie@7596ff.com>
2016-11-04 05:32:13 -0400

    add another autorestart trigger
    also test committing from github

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 // node-dota2 stuff
 var steam = require('steam'),
-	util = require('util'),
-	fs = require('fs'),
-	crypto = require('crypto'),
-	dota2 = require('dota2');
+    util = require('util'),
+    fs = require('fs'),
+    crypto = require('crypto'),
+    dota2 = require('dota2');
 
 var steamClient = new steam.SteamClient(),
-	steamUser = new steam.SteamUser(steamClient),
-	steamFriends = new steam.SteamFriends(steamClient),
-	Dota2 = new dota2.Dota2Client(steamClient, true, true);
+    steamUser = new steam.SteamUser(steamClient),
+    steamFriends = new steam.SteamFriends(steamClient),
+    Dota2 = new dota2.Dota2Client(steamClient, true, true);
 
 // load config
 global.config = require('./config');
@@ -20,19 +20,19 @@ const token = global.config.discord_token;
 
 // sendmessage easier
 var sendToDota = function(msg) {
-	Dota2.sendMessage(global.config.bot_channel, msg, global.config.bot_channel_type);
+    Dota2.sendMessage(global.config.bot_channel, msg, global.config.bot_channel_type);
 }
 
 // do send message stuff
 var dualMessage = function(msg) {
-	bot.channels.get(global.config.discord_listen_channel).sendMessage('`' + msg + '`');
-	sendToDota(msg);
+    bot.channels.get(global.config.discord_listen_channel).sendMessage('`' + msg + '`');
+    sendToDota(msg);
 };
 
 var gracefulRestart = function() {
-	console.log('restarting.');
-	dualMessage('Restarting.');
-	setTimeout(function() {process.exit();}, 2000);
+    console.log('restarting.');
+    dualMessage('Restarting.');
+    setTimeout(function() {process.exit();}, 2000);
 }
 
 var discordParseEmoji = function(message) {
@@ -40,58 +40,63 @@ var discordParseEmoji = function(message) {
 };
 
 var onSteamLogOn = function onSteamLogOn(logonResp) {
-	if (logonResp.eresult == steam.EResult.OK) {
-		steamFriends.setPersonaState(steam.EPersonaState.Busy);
-		steamFriends.setPersonaName(global.steam_name);
-		util.log('Logged on.');
-		Dota2.launch();
-		Dota2.on('ready', function() {
-			// do chat logic
+    if (logonResp.eresult == steam.EResult.OK) {
+        steamFriends.setPersonaState(steam.EPersonaState.Busy);
+        steamFriends.setPersonaName(global.steam_name);
+        util.log('Logged on.');
+        Dota2.launch();
+        Dota2.on('ready', function() {
+            // do chat logic
 
-			Dota2.joinChat(global.config.bot_channel, global.config.bot_channel_type);
+            Dota2.joinChat(global.config.bot_channel, global.config.bot_channel_type);
 
-			console.log('Node-dota2 ready.');
-		});
+            console.log('Node-dota2 ready.');
+        });
 
-		Dota2.on('unready', function onUnready() {
-			console.log('Node-dota2 unready.');
-		});
-		Dota2.on('chatMessage', function(channel, personaName, message) {
-			console.log('message from dota: ' + personaName + ': ' + message);
-			bot.channels.get(global.config.discord_listen_channel).sendMessage('**' + personaName + ':** ' + message);
-		});
-		Dota2.on('unhandled', function(kMsg) {
-			util.log('UNHANDLED MESSAGE' + kMsg);
-		});
+        Dota2.on('unready', function onUnready() {
+            console.log('Node-dota2 unready.');
+        });
+        Dota2.on('chatMessage', function(channel, personaName, message) {
+            console.log('message from dota: ' + personaName + ': ' + message);
+            bot.channels.get(global.config.discord_listen_channel).sendMessage('**' + personaName + ':** ' + message);
+        });
+        Dota2.on('unhandled', function(kMsg) {
+            util.log('UNHANDLED MESSAGE' + kMsg);
+        });
 
-		Dota2.on('hellotimeout', function() {
-			gracefulRestart();
-		});
+        Dota2.on('hellotimeout', function() {
+            gracefulRestart();
+        });
 
-		bot.on('message', message => {
-			if (message.channel.name == 'club-purple' && message.member.user.id != global.config.discord_self_id) {
-				console.log('message from discord: ' + message.member.user.username + ': ' + message.content);
-				sendToDota(message.member.user.username + ': ' + message.content);
+        bot.on('message', message => {
+            if (message.channel.name == 'club-purple' && message.member.user.id != global.config.discord_self_id) {
+                console.log('message from discord: ' + message.member.user.username + ': ' + message.content);
+                sendToDota(message.member.user.username + ': ' + message.content);
 
-				if (message.content === "!restart") {
-					gracefulRestart();
-				}
-			}
-		});
-	}
+                if (message.content === "!restart") {
+                    gracefulRestart();
+                }
+            }
+        });
+    }
 };
 
 var onSteamServers = function onSteamServers(servers) {
-	util.log('Recieved servers.');
-	fs.writeFile('servers', JSON.stringify(servers));
+    util.log('Recieved servers.');
+    fs.writeFile('servers', JSON.stringify(servers));
 };
 
 var onSteamLogOff = function onSteamLogOff(eresult) {
-	util.log('Logged off from steam.');
+    util.log('Logged off from steam.');
 };
 
 var onSteamError = function onSteamError(error) {
+<<<<<<< HEAD
 	util.log('Connection closed by server.');
+	gracefulRestart();
+=======
+    util.log('Connection closed by server.');
+>>>>>>> 0caa001... tabs to spaces
 };
 
 steamUser.on('updateMachineAuth', function(sentry, callback) {
@@ -105,7 +110,7 @@ steamUser.on('updateMachineAuth', function(sentry, callback) {
 
 // discord
 bot.on('ready', () => {
-	console.log('discord ready.');
+    console.log('discord ready.');
 });
 
 // log in, no auth code needed
@@ -115,7 +120,7 @@ var logOnDetails = {
 };
 
 if (global.config.steam_guard_code) {
-	logOnDetails.auth_code = global.config.steam_guard_code;
+    logOnDetails.auth_code = global.config.steam_guard_code;
 }
 
 try {
@@ -127,7 +132,7 @@ try {
 
 steamClient.connect();
 steamClient.on('connected', function() {
-	steamUser.logOn(logOnDetails);
+    steamUser.logOn(logOnDetails);
 });
 steamClient.on('logOnResponse', onSteamLogOn);
 steamClient.on('loggedOff', onSteamLogOff);
